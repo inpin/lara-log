e7215b7ada42ae32511126f47df8b975fce0c8d8
Mohammad Nourinik <enourinick@gmail.com>
2018-06-28 15:49:24 +0000

    Apply fixes from StyleCI

--- src/CaptureApiActionMiddleware.php
+++ src/CaptureApiActionMiddleware.php
@@ -10,18 +10,19 @@ class CaptureApiActionMiddleware
     /**
      * Create a log from the incoming request.
      *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Closure $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         ApiActionLog::query()->create([
-            'user_id' => auth('api')->id(),
+            'user_id'    => auth('api')->id(),
             'route_name' => Route::currentRouteName(),
-            'method' => $request->method(),
-            'uri' => $request->getUri(),
-            'body' => json_encode($request->all()),
+            'method'     => $request->method(),
+            'uri'        => $request->getUri(),
+            'body'       => json_encode($request->all()),
         ]);
 
         return $next($request);
